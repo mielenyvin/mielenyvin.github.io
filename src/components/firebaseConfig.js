dbbb7df6419183f1d6215f80262d8154a10a08c1
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2021-08-14 00:10:58 +0200

    initial

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCfDm-2XxVzJXZgR95tHsUHr0Lbsq7LRXg",
+  authDomain: "test-29193.firebaseapp.com",
+  projectId: "test-29193",
+  storageBucket: "test-29193.appspot.com",
+  messagingSenderId: "619446844161",
+  appId: "1:619446844161:web:a601201f0c35f7b308b900"
+};
