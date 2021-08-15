bb8e621c9c1d08f2b7f3ce77c331db795cbb581e
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2021-08-15 17:05:51 +0200

    Update NewEmployee.vue

--- src/components/NewEmployee.vue
+++ src/components/NewEmployee.vue
@@ -1,30 +1,30 @@
 <template>
   <div id="new-employee">
-    <h3>New Employee</h3>
+    <h3>New Truck</h3>
     <div class="row">
     <form @submit.prevent="saveEmployee" class="col s12">
       <div class="row">
         <div class="input-field col s12">
           <input type="text" v-model="employee_id" required>
-          <label>Employee ID#</label>
+          <label>Truck ID#</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
           <input type="text" v-model="name" required>
-          <label>Name</label>
+          <label>Producer</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
           <input type="text" v-model="dept" required>
-          <label>Department</label>
+          <label>Model</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
           <input type="text" v-model="position" required>
-          <label>Position</label>
+          <label>Fuel consumption (per 100 km)</label>
         </div>
       </div>
       <button type="submit" class="btn">Submit</button>
